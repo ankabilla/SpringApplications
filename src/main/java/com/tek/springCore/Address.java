11f14f43f6b8d4da525e097f81f12078d773524f
ankabilla <ankabilla1990@gmail.com>
2017-05-31 17:20:42 -0400

    Initial Commit

---
+++ src/main/java/com/tek/springCore/Address.java
@@ -0,0 +1,18 @@
+package com.tek.springCore;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Address {
+
+	
+	private String city;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
